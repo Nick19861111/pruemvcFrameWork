dada1268785335b8efa22021f9dfcab3aff28e29
Nick19861111 <114399678+Nick19861111@users.noreply.github.com>
2024-07-18 00:10:02 +0800

    1

--- assets/Script/framework/manager/HttpManager.ts
+++ assets/Script/framework/manager/HttpManager.ts
@@ -1,8 +1,9 @@
+import Gloab from "../../Gloab";
 
 /**
  * http的相关操作
  */
-export default class HttpManager  {
+export default class HttpManager {
 
     /**
     * post的方式
@@ -25,18 +26,38 @@ export default class HttpManager  {
         xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
         xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
+            if (xhr.readyState === 4) { // 请求完成
                 let response = xhr.responseText;
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    let httpStatus = xhr.statusText;
-                    // callback(true, JSON.parse(response));
-                    callback(true, response);
-
-                } else {
-                    callback(false, response);
+                if (xhr.status >= 200 && xhr.status < 300) { // 请求成功
+                    try {
+                        // 尝试解析 JSON 响应
+                        let jsonResponse = JSON.parse(response);
+                        callback(true, jsonResponse);
+                    } catch (e) {
+                        // JSON 解析失败
+                        callback(false, "Response parsing error: " + e.message);
+                    }
+                } else { // 请求失败
+                    callback(false, JSON.stringify({ code: 1 }));
+                    //Gloab.Utils.invokeCallback(callback, 1);
                 }
             }
         };
+
+        //处理请求错误
+        xhr.onerror = function () {
+            callback(false, JSON.stringify({ code: 1 }));
+            //Gloab.Utils.invokeCallback(callback, 1);
+        }
+
+        xhr.ontimeout = function () {
+            callback(false, JSON.stringify({ code: 1 }));
+            console.log("请求超时");
+            //Gloab.Utils.invokeCallback(callback, 1);
+        }
+
+        xhr.timeout = 5000;
+
         xhr.send(JSON.stringify(param));
         return xhr;
     }
