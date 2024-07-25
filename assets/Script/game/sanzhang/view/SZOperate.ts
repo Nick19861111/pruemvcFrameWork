f06d782213f3ddd967a2a42b22450a70e21f15bc
Nick19861111 <114399678+Nick19861111@users.noreply.github.com>
2024-07-25 13:56:26 +0800

    添加了按钮的相关控制类

---
+++ assets/Script/game/sanzhang/view/SZOperate.ts
@@ -0,0 +1,31 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+/**
+ * 添加按钮的相关控制
+ */
+
+@ccclass
+export default class SZOperate extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
