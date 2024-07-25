0a35b9798c0d6964d5fd7ee5e3b20085937e8163
Nick19861111 <114399678+Nick19861111@users.noreply.github.com>
2024-07-25 14:14:17 +0800

    添加聊天管理类，按钮管理类，对比的管理类

---
+++ assets/Script/game/sanzhang/view/SZChat.ts
@@ -0,0 +1,24 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+/**
+ * 三张游戏的聊天管理类
+ */
+
+@ccclass
+export default class SZChat extends cc.Component {
+
+    onLoad () {
+
+    }
+
+    onDestroy(): void {
+        
+    }
+}
